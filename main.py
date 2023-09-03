c5b63b1992aab26c99a78fc30b112a2d19dd632c
Alec Miller <alecmichaelmiller@gmail.com>
2023-09-02 18:23:11 -0700

    removed previous

--- main.py
+++
@@ -1,5 +0,0 @@
-from scoring import score
-from setup import setup
-
-setup()
-score()
--- scripts/buildWindows.bat
+++
@@ -1,2 +0,0 @@
-python -m nuitka --include-data-dir=resources=resources --standalone main.py
-"%programfiles(x86)%\Inno Setup 6\iscc.exe" "scripts\innoscript.iss" "/DMyAppVersion=%1"
