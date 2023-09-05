323b99af355426526330830e3a67565ca450e077
Alec Miller <alecmichaelmiller@gmail.com>
2023-09-05 16:52:06 -0700

    selects award winner

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
+from reader.awards.pickWinner import selectAwardWinner
 from reader.awards.selectAward import selectAward
 from reader.checkIn.checkin import checkIn
 from reader.skills.addSkills import enterSkillsScore
 
 TEAM_LIST = [str(i) for i in range(1, 25)]
 
-selectAward('judges award')
+selectAwardWinner('judges award', '4', TEAM_LIST)
---
+++ reader/awards/pickWinner.py
@@ -0,0 +1,35 @@
+from interaction.clicker import clickAt
+from reader.awards.selectAward import selectAward
+from reader.base.reader import getRegion
+
+DROPDOWN_X = 731
+DROPDOWN_Y = 152
+
+REGION_TOP_LEFT = [597, 180]
+REGION_BOTTOM_RIGHT = [743, 759]
+
+SAVE_X = 776
+SAVE_Y = 340
+
+def selectAwardWinner(award: str, winner: str, teams: list[str]):
+    selectAward(award)
+    clickAt(DROPDOWN_X, DROPDOWN_Y)
+    choices =  getRegion([REGION_TOP_LEFT, REGION_BOTTOM_RIGHT])
+    firstMatch = None
+    lastMatch = None
+    for choice in choices:
+        if choice.text in teams:
+            if not firstMatch:
+                firstMatch = teams.index(choice.text), choice.y
+            else:
+                lastMatch = teams.index(choice.text), choice.y
+
+    if not lastMatch:
+        raise RuntimeError(f"Could not parse team list")
+    
+    scale = (lastMatch[1] - firstMatch[1]) / (int(lastMatch[0]) - int(firstMatch[0]))
+    index = teams.index(winner)
+    winnerPosition = firstMatch[1] + (index - int(firstMatch[0])) * scale
+    clickAt(DROPDOWN_X, winnerPosition)
+
+    clickAt(SAVE_X, SAVE_Y)
